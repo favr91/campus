0764aca1d66959938fd30194d3ac1c9eeb911153
Francisco Vasquez <panchov91@gmail.com>
2017-02-10 00:48:48 -0400

    mejorado navbar y navbar, falta mas estilos

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-end
+ end
